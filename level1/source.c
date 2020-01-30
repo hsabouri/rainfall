17665896271b8beb84f11fe94b43e338fa93ae34
Hugo Sabourin <hsabouri@student.42.fr>
2020-01-30 10:25:04 +0100

    level2 research

--- level1/source.c
+++ level1/source.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+int run(void)
+{
+    fprint("Good.. Wait what ?\n", 0x13, 1, 1);
+    system("/bin/sh");
+}
+
 int main(void)
 {
     char buffer[76];
 
-    gets()
+    gets(buffer);
     return 0;
 }
