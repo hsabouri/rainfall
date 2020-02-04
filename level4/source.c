7d3adc70c92ab31f9abeb950e19eef39bbc920a6
Hugo SABOURIN <hsabouri@student.42.fr>
2020-02-04 14:57:42 +0100

    level04

---
+++ level4/source.c
@@ -0,0 +1,25 @@
+int m = 0;
+
+void p(char buff[])
+{
+    printf(buff);
+}
+
+void n(void)
+{
+    char    buff[520];
+
+    fgets(buff, 512, 1);
+    p(buff);
+    if (m == 0x01025544) // \x10\x98\x04\x08
+    {
+        system("/bin/cat /home/user/level5/.pass");
+    }
+    return ;
+}
+
+int main(void)
+{
+    n();
+    return (0);
+}
