581e549decc0fd60d7537595d1cef11cd4671893
Adrien MORDRET <amordret@e1r9p5.42.fr>
2020-02-18 16:49:13 +0100

    source lvl7

--- level7/source.c
+++ level7/source.c
@@ -1,3 +1,13 @@
+void    m(void)
+{
+    time_t  t;
+
+    t = time();
+    printf("%s - %d\n", g_c, t);
+    return ;
+}
+
+
 int main(int ac, char **av)
 {
     int *a = malloc(8);
@@ -5,14 +15,18 @@ int main(int ac, char **av)
 
     int *b = malloc(8);
     *b = 1; // = *a
-
+    a[1] = b;
     int *c = malloc(8);
     *c = 2;
 
     b = malloc(8);
-    *b = 2; // = *c;
-
-
-
+    c[1] = b;
+    
+    strcpy(a[1], av[1]);
+    strcpy(c[1], av[2]);
+    
+    g_stream = fopen("/home/user/level8/.pass", "r");
+    fgets(g_c, 68, g_stream);
+    puts("~~");
     return (0);
 }
