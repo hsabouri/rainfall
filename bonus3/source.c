20e1119e04a0020ece23c608e35d6921981a9818
Hugo Sabourin <hsabouri@student.42.fr>
2020-03-07 17:09:54 +0100

    final walkthrough

--- bonus3/source.c
+++ bonus3/source.c
@@ -2,5 +2,27 @@
 
 int main(int ac, char **av)
 {
-    FILE *pass = fopen("/home/user/end/.pass", "r");
+    char    ptr[132];
+    int     i = 0;
+    int     size;
+    FILE    *pass;
+
+    pass = fopen("/home/user/end/.pass", "r");
+    while (i++ < 33)
+        ((int *)ptr)[i] = 0;
+    if (pass)
+    {
+        if (ac == 2)
+        {
+            fread(ptr, 1, 66, pass);
+            i = atoi(av[1]);
+            ptr[i] = 0;
+            fread(ptr + 66, 1, 65, pass);
+            fclose(pass);
+            if (strcmp(ptr, av[1]) == 0)
+                execl("/bin/sh", "sh", 0);
+            else
+                puts(ptr + 66);
+        }
+    }
 }
